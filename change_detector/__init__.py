36e6b5813c1c6fee3a05bf9cb99138272f8ac14b
corka149 <corka149@mailbox.org>
2018-09-19 18:25:56 +0200

    Added file_syncer to module structure

--- change_detector/__init__.py
+++ change_detector/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ["core", "client_server"]
+__all__ = ["core", "client_server", "file_syncer"]
 from . import core
 from . import client_server
+from . import file_syncer
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='change_detector',
     version='0.6',
-    packages=['change_detector', 'change_detector.client_server'],
+    packages=['change_detector', 'change_detector.client_server', 'change_detector.file_syncer'],
     url='https://github.com/corka149/change_detector',
     license='MIT',
     author='Corka149',
